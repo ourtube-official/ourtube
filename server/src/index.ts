ea6ff99694c9d0b8f650be31d9cf4fdb37be9585
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-07-24 15:51:16 -0700

    🎉 Initial Commit

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+
+import express from "express";
+import mongoose from "mongoose";
+
+import logger from "./utils/logger";
+
+const app = express();
+app.get("/", (_, res) => res.send("Hello World!"));
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    },
+    (err) => {
+        if (err) logger.error(err);
+        logger.info("Successfully connected to MongoDB!");
+    }
+);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+    logger.info(`Server successfully listening on port ${PORT}`)
+);
---
+++ server/src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        CLOUDINARY_URL: string;
+        MONGODB_URI: string;
+    }
+}
